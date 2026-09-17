6dc93993595db5f3270a7adde18df5de4dca8ac8
Kruthik B T <kruthikchinnu641@gmail.com>
2026-09-17 09:59:46 +0530

    Add files via upload

---
+++ Question1.js
@@ -0,0 +1,10 @@
+//Calculate teh average marks of four students and print the average
+
+let sum=0;
+let marks=[56,78,79,80];
+for(let i=0;i<marks.length;i++){
+    sum += marks[i];
+}
+
+console.log(`The Total Sum is ${sum}`);  //String Literal
+console.log(`The Average marks is ${sum/marks.length}`);
