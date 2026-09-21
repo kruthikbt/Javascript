389437ad657e7d5ef3f74388061c5994f7a4c45d
Kruthik BT <kruthikchinnu641@gmail.com>
2026-09-21 23:52:09 +0530

    nee had

---
+++ Functions/function.js
@@ -0,0 +1,8 @@
+function myFunction() {   // Function definition by using function keyword
+
+    console.log("Hello World!");
+    console.log("I love Web Developent!");
+
+}
+
+myFunction(); // Function Call..
