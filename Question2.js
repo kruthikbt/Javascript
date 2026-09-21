e1e10eceb09cb91bd71b456a8dbc8a3220596d4c
Kruthik BT <kruthikchinnu641@gmail.com>
2026-09-21 23:37:10 +0530

    Add new folder

--- Question2.js
+++
@@ -1,8 +0,0 @@
-// Crete an array of items of 5 items of price and  add the 10% discount and print teh final amount of theitem
-
-let items =[200,300,400,500];
-for(let i=0;i< items.length;i++){
-    let offer = items[i]/10;
-    items[i] -= offer;
-}
-console.log(items);
