e62dead5e590d45cabdfdfb580794efed84ecacd
Kruthik B T <kruthikchinnu641@gmail.com>
2026-09-24 01:12:19 +0530

    Add files via upload

---
+++ Minor_Projects/Largest_No_Checker/lagest_no.js
@@ -0,0 +1,12 @@
+let arr=[20,30,40,50];
+function largest(arr){
+    let largest = arr[0];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] < smallest){
+            largest = arrlargest
+    }
+    return largest;
+}
+}
+
+console.log(`largest number is : ${largest(arr)}`)
